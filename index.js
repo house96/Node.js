ea7e937eb2a1b01c1f677cb1c5cb5db1325f16c1
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-11-26 21:35:38 +0200

    node server

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-function Car(carNeme) {
-    this.carNeme = carNeme || 'unknown name';
+var http = require('http');
+var url = require('url');
+
+var fs = require('fs');
+
+var server = new http.Server;
+
+server.listen(8080, '127.0.0.1'); // localhost
+
+server.on('request', function(req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    fs.readFile(getPegeNemeByPath(parsedUrl.pathname) + '.html', function(err, data){
+        if (err) throw new Error(err);
+        res.end(data);
+    })
+
+
+});
+
+function getPegeNemeByPath(path) {
+    switch (path) {
+        case '/':
+        case '/home':
+            return 'index';
+        case '/about':
+            return 'about';
+        default: return 'error';
+    }
 }
-
-Car.prototype.logNeme = function() {
-    console.log('Car name is: ', this.carNeme);
-};
-
-var bmw = new Car('BMW');
-bmw.logNeme();
